270f080fc2e3653684ba54c33cbdc41f2bff88b2
Justin Yoo <justin.yoo@aliencube.com>
2013-08-12 23:28:12 +1000

    Added configuration key for boilerplate template path.

--- SourceCodes/Boilerplate.Builder.Services.Utilities/Settings.cs
+++ SourceCodes/Boilerplate.Builder.Services.Utilities/Settings.cs
@@ -1,4 +1,10 @@
-﻿namespace Boilerplate.Builder.Services.Utilities
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Boilerplate.Builder.Services.Utilities
 {
 	/// <summary>
 	/// This represents the application configuration settings entity.
@@ -37,9 +43,51 @@
 			}
 		}
 
+		/// <summary>
+		/// Gets the directory path where boilerplate projects solution file is.
+		/// </summary>
+		public string BoilerplatePath
+		{
+			get
+			{
+				var value = ConfigurationManager.AppSettings["App.BoilerplatePath"];
+				var path = GetFullPath(value);
+				return path;
+			}
+		}
+
 		#endregion Properties
 
 		#region Methods
+
+		/// <summary>
+		/// Gets the full path of the given application root path.
+		/// </summary>
+		/// <param name="path">Application root path.</param>
+		/// <returns>Returns the full path of the given application root path.</returns>
+		private static string GetFullPath(string path)
+		{
+			//	Returns NULL, if the given path is NULL or empty.
+			if (String.IsNullOrWhiteSpace(path))
+				return null;
+
+			//	Returns the original path, if the given path is UNC.
+			if (Regex.IsMatch(path, @"[a-z]:\\", RegexOptions.Compiled | RegexOptions.IgnoreCase))
+				return path;
+
+			//	Sets the given path to be application root.
+			if (!path.StartsWith("/"))
+				path = "/" + path;
+			if (!path.StartsWith("~"))
+				path = "~" + path;
+
+			var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			if (!String.IsNullOrEmpty(directory))
+				path = String.Format("{0}\\{1}",
+				                     directory.TrimEnd('/', '\\'),
+				                     path.Replace("~/", "").Replace("/", "\\").Trim('/', '\\'));
+			return path;
+		}
 
 		#endregion Methods
 	}
