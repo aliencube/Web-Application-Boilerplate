a97a6adc22434d2aaa7f5fc4a05f08bb1189202a
Justin Yoo <justin.yoo@aliencube.com>
2013-08-26 12:40:37 +1000

    Added jquery.validate plugin.

--- SourceCodes/Boilerplates/Application.Web.UI/App_Start/BundleConfig.cs
+++ SourceCodes/Boilerplates/Application.Web.UI/App_Start/BundleConfig.cs
@@ -14,10 +14,18 @@ namespace Application.Web.UI.App_Start
 
 			//	Javascript bundles.
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-2.6.2.js"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js",
+			                                                                     "~/Scripts/jquery.validate.js",
+			                                                                     "~/Scripts/jquery.validate.unobtrusive.js",
+			                                                                     "~/Scripts/jquery.unobtrusive-ajax.js",
+			                                                                     "~/Scripts/jquery.cookie.js"));
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/es5-shim").Include("~/Scripts/es5-shim.js"));
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/placeholders").Include("~/Scripts/Placeholders.js", "~/Scripts/jquery.Placeholders.monkey.patch.js"));
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/linq").Include("~/Scripts/linq.js", "~/Scripts/linq.jquery.js"));
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Scripts/plugins.js", "~/Scripts/main.js"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/placeholders").Include("~/Scripts/Placeholders.js",
+			                                                                           "~/Scripts/jquery.Placeholders.monkey.patch.js"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/linq").Include("~/Scripts/linq.js",
+			                                                                   "~/Scripts/linq.jquery.js"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Scripts/plugins.js",
+			                                                                 "~/Scripts/main.js"));
 		}
 	}
 }
